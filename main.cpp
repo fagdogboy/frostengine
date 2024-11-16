03df4f6bbf35f5be9b8cd20acf9a8dea027312f0
snowy <150125982+SnowyBoop@users.noreply.github.com>
2024-11-16 22:18:44 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,26 +6,69 @@
 #include <cstdlib>
 #include <GLFW/glfw3.h>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
-    void run() {
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
-
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+  
 private:
-    void initVulkan() {
+  GLFWwindow* window;
 
+  VKInstance instance;
+
+  
+  
+  void createInstance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+  }
+  
+  void initWindow() {  
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    
+  }
+
+  void initVulkan() {
+    createInstance();
+  }
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
     }
+  }
 
-    void mainLoop() {
+  void cleanup() {
 
-    }
+    glfwDestroyWindow(window);
 
-    void cleanup() {
+    glfwTerminate();
+    
+  }
 
-    }
 };
 
 int main() {
